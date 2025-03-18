5c96e9f9517d39a6443ca7325ff9bb808f043ca0
Michelle <mpuschkarow@web.de>
2025-03-18 19:40:26 +0100

    styles & burger menu

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 function renderContent(){
     for (let i = 0; i < dishes.length; i++) {
-        document.getElementById('div_content').innerHTML += templateContent(i);
-        
+        if(dishes[i].dish == "main"){
+            document.getElementById('main_dishes').innerHTML += templateMainDish(i);
+        } else if(dishes[i].dish == "side"){
+            document.getElementById('side_dishes').innerHTML += templateSideDish(i);
+        } else if(dishes[i].dish == "drink"){
+            document.getElementById('drinks').innerHTML += templateDrinks(i);
+        }
     }
 }
 
 function addToBasket(i){
-    document.getElementById('div_basket').innerHTML += templateBasket(i);
+    document.getElementById('div_basket_content').innerHTML += templateBasket(i);
+    document.getElementById('div_basket_content').innerHTML += templateTotalPrice();
+
+}
+
+function deleteBasket(){
+    document.getElementById(`div_basket_content`).innerHTML = "";
+}
+
+function changeAmount(a, i){
+    if(a > 0){
+        document.getElementById(`amount${i}`).innerHTML = `${+1}`;
+    } else if(a < 0){
+        document.getElementById(`amount${i}`).innerHTML = `${-1}`;
+    }
+
+}
+
+function deliveryOrPickup(a, event){
+    event.target.classList.toggle('choosed_btn');
+    event.target.classList.toggle('not_choosed_btn');
 }
--- script/template.js
+++ script/template.js
@@ -1,13 +1,81 @@
-function templateContent(i){
+function triggerNavbar(){
+   document.getElementById('navbar').classList.toggle('d_none');
+
+}
+
+function templateMainDish(i){
     return /*html*/`
         <div>
             <p>${dishes[i].image}</p>
             <div class="div_single_dish">
                 <h3>${dishes[i].name}</h3>
-                <button id="add_to_basket" class="add_to_basket" onclick="addToBasket(i)">+</button>
+                <button id="add_to_basket" class="add_to_basket hover" onclick="addToBasket(${i})">+</button>
             </div>
             <p>${dishes[i].content}</p>
             <p>${dishes[i].price} €</p>
         </div>
     `
+}
+
+function templateSideDish(i){
+    return /*html*/`
+        <div>
+            <p>${dishes[i].image}</p>
+            <div class="div_single_dish">
+                <h3>${dishes[i].name}</h3>
+                <button id="add_to_basket" class="add_to_basket hover" onclick="addToBasket(${i})">+</button>
+            </div>
+            <p>${dishes[i].content}</p>
+            <p>${dishes[i].price} €</p>
+        </div>
+    `
+}
+
+function templateDrinks(i){
+    return /*html*/`
+        <div>
+            <p>${dishes[i].image}</p>
+            <div class="div_single_dish">
+                <h3>${dishes[i].name}</h3>
+                <button id="add_to_basket" class="add_to_basket hover" onclick="addToBasket(${i})">+</button>
+            </div>
+            <p>${dishes[i].content}</p>
+            <p>${dishes[i].price} €</p>
+        </div>
+    `
+}
+
+function templateBasket(i){
+    return /*html*/`
+    <div>
+        <div class="div_single_dish">
+            <h3>${dishes[i].name}</h3>
+            <div class="div_plus_minus">
+            <button id="plus_amount" class="change_amount_btn hover" onclick="changeAmount(+1, ${i})">+</button>
+            <div id="amount${i}">1x</div>
+            <button id="minus_amount" class="change_amount_btn hover" onclick="changeAmount(-1, ${i})">-</button>
+            </div>
+        </div>
+        <p>${dishes[i].price} €</p>
+    </div>
+`
+}
+
+function templateTotalPrice(){
+    return /*html*/`
+    <div class="div_total_price">
+        <div class="div_current_sum">
+            <p>Zwischensumme</p>
+            <p>€</p>
+        </div>
+        <div class="div_delivery_costs">
+            <p>Lieferkosten</p>
+            <p id="delivery_cost">5 €</p>
+        </div>
+        <div class="div_total_sum">
+            <p>Gesamt</p>
+            <p>€</p>
+        </div>
+    </div>
+`
 }
