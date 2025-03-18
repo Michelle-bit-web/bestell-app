efdf0abfc25fe09fab6ec2e3930ddd351025c6ec
Michelle <mpuschkarow@web.de>
2025-03-18 15:46:03 +0100

    json-array added

--- script/db.js
+++ script/db.js
@@ -1,3 +1,65 @@
 let dishes = [
-    {},{},{},{},{},{},{},{},
+    {
+        "name": "Pizza Sandwich",
+        "dish": "main",
+        "price": 9.50,
+        "content": "Hausgemachter Pizzateig im Steinofen mit Tomaten, Mozzarella und Basilikum."
+    },
+    {
+        "name": "Grilled Chicken",
+        "dish": "main",
+        "price": 12.90,
+        "content": "Zarte Hähnchenbrust vom Grill. Dazu Semmelklöße und frische Champignons und Dill-Sauce."
+    },
+    {
+        "name": "Basilikum Spätzle",
+        "dish": "main",
+        "price": 10.40,
+        "content": "Hausgemachte Eierspätzle mit Basilikum und weiteren Kräutern - mit Käse überbacken "
+    },
+    {
+        "name": "Pizza Sandwich",
+        "dish": "main",
+        "price": 9.50,
+        "content": "Hausgemachter Pizzateig im Steinofen mit Tomaten, Mozzarella und Basilikum."
+    },
+    {
+        "name": "Hackfresse",
+        "dish": "main",
+        "price": 9.50,
+        "content": "Große Tortilla-Tasche mit Hackfleisch, Mais, Paprika, Tomate und Feta gefüllt."
+    },
+    {
+        "name": "Potato Fries",
+        "dish": "side",
+        "price": 4.50,
+        "content": "Hausgemachter Pizzateig im Steinofen mit Tomaten, Mozzarella und Basilikum."
+    },
+    {
+        "name": "Sweet Potato Fries",
+        "dish": "side",
+        "price": 4.50,
+        "content": "Hausgemachter Pizzateig im Steinofen mit Tomaten, Mozzarella und Basilikum."
+    },
+    {
+        "name": "Gartensalat",
+        "dish": "side",
+        "price": 9.50,
+        "content": "Klassischer Romansalat mit Tomaten, Paprika, roten Zwiebeln und einem Balsamico-Dressing."
+    },
+    {
+        "name": "Basilikum-Zitrone-Limonade",
+        "dish": "drink",
+        "price": 1.20,
+    },
+    {
+        "name": "Himbeer-Minze-Limonade",
+        "dish": "drink",
+        "price": 1.20,
+    },
+    {
+        "name": "Cola",
+        "dish": "drink",
+        "price": 1.20,
+    }
 ]
